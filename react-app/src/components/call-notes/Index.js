a6425b9d5320de3b7259793962a6507502a64740
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2023-06-29 12:31:38 -0600

    Notes history can now be cleared.

--- react-app/src/components/call-notes/Index.js
+++ react-app/src/components/call-notes/Index.js
@@ -12,12 +12,19 @@ const Index = () => {
       setHistory(JSON.parse(localStorage.getItem('history')))
     }
   }, [])
+
+  const clearHistory = () => {
+    localStorage.clear()
+    setHistory([])
+  }
   
-  console.log('history: ', history)
 
   return(
     <div className='notes-index-container'>
-      <h1>History</h1>
+      <div className='notes-index-header'>
+        <h1>History</h1> 
+        <button type='button' onClick={() => clearHistory()}>clear history</button>
+      </div>
       <ul>
         {history.map(note => <li key={note.createdAt}><Show createdAt={note.createdAt} caller={note.caller} callBackNumber={note.callBackNumber} address={note.address} issues={note.issues} troubleshooting={note.troubleshooting} resolution={note.resolution} /></li>)}
       </ul>
